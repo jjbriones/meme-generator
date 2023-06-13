e7eee48a313483c79d665683abfb5ab38e2c981b
Jarl Briones <108669333+jjbriones@users.noreply.github.com>
2023-06-13 13:17:33 +0800

    "fixed img header"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 export default function Header() {
-    return (
-        <header className="header">
-            <img 
-                src="./images/troll-face.png" 
-                className="header--image"
-            />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
-        </header>
-    )
+  return (
+    <header className="header">
+      <img
+        src={require('../troll-face.png')}
+        className="header--image"
+        alt="troll-face"
+      />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React - Project</h4>
+    </header>
+  );
 }
